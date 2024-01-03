c90686d80b8a9cc9fa90bf8e9133412ef7c24044
Ajay v <vajay4834@gmail.com>
2024-01-03 19:13:02 +0530

    completed

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,8 +71,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatapp.wsgi.application'
-
-
+ASGI_APPLICATION = "chatapp.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {"BACKEND": "channels.layers.InMemoryChannelLayer"},
+}
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
